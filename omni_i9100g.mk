21fbe23a2df522883e25f7990a5091991cf907e1
Markus Guidry <markus@gu1dry.com>
2014-03-09 15:46:47 -0400

    i9100g: device makefile cleanup
    -Removes the redundant aosp_i9100g.mk & full_i9100g.mk.
    -Standardizes the layout of omni_i9100g.mk
    -Adds a README
    
    Change-Id: Id169efdc0981a5384f996aa831c15a553780ce64

--- omni_i9100g.mk
+++ omni_i9100g.mk
@@ -15,22 +15,28 @@
 # limitations under the License.
 #
 
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit Omni GSM telephony parts
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
-# Inherit from hardware-specific part of the product configuration
+# Inherit some common Omni stuff
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit more device specific configurations
 $(call inherit-product, device/samsung/i9100g/device.mk)
 
 # Discard inherited values and use our own instead.
+PRODUCT_MODEL := GT-I9100G
+PRODUCT_BRAND := samsung
 PRODUCT_NAME := omni_i9100g
 PRODUCT_DEVICE := i9100g
-PRODUCT_BRAND := samsung
-PRODUCT_MANUFACTURER := Samsung
-PRODUCT_MODEL := GT-I9100G
+PRODUCT_MANUFACTURER := samsung
 
 #Set build fingerprint / ID / Prduct Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-I9100G TARGET_DEVICE=GT-I9100G BUILD_FINGERPRINT=samsung/GT-I9100G/GT-I9100G:4.1.2/JZO54K/I9100GXXLSR:user/release-keys PRIVATE_BUILD_DESC="GT-I9100G-user 4.1.2 JZO54K I9100GXXLSR release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=GT-I9100G \
+    TARGET_DEVICE=GT-I9100G \
+    PRIVATE_BUILD_DESC="GT-I9100G-user 4.1.2 JZO54K I9100GXXLSR release-keys" \
+    BUILD_FINGERPRINT="samsung/GT-I9100G/GT-I9100G:4.1.2/JZO54K/I9100GXXLSR:user/release-keys"
