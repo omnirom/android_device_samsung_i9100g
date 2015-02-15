5045f190f8e31d5e1a51b2719ca18c388a7788be
Michael Gernoth <michael@gernoth.net>
2015-02-15 21:06:22 +0100

    media_codecs: fix Netflix
    The current Netflix app tries very hard to find a codec which supports
    AdaptivePlayback. As our HW-codec doesn't support that, the Google
    H.264 software-codec gets used. But it is incapable of decoding the
    stream from Netflix, returning H264SWDEC_STRM_ERR. The hw-codec
    plays the stream just fine.
    This removes the declaration of adaptive-playback from the (broken)
    software-codec which causes Netflix to use hw-based decoding.
    
    Change-Id: I50b33f445a7db5bd7ad59d200277c193c68e3ba7

--- device.mk
+++ device.mk
@@ -65,9 +65,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
+    $(LOCAL_PATH)/configs/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml
 
 
 # Packages
