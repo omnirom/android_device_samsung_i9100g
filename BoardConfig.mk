01b5b7d1d6df6b97ecdd3db5b60ca42eeb4199cf
Jiangyi <sam.andrew.jiang@gmail.com>
2015-03-22 09:55:51 -0400

    i9100g: Update SELinux Policies [2/2]
    Move common policies to omap4-common, add device-specific policies.
    
    Change-Id: I3c10ee19664561ad5fc3e14e67ddca68daddd3a0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -112,17 +112,15 @@ BOARD_BLUEDROID_VENDOR_CONF := device/samsung/i9100g/bluetooth/vnd_i9100g.txt
 BOARD_USES_SECURE_SERVICES := true
 
 # Selinux
-BOARD_SEPOLICY_DIRS := \
-    device/samsung/i9100g/selinux
+BOARD_SEPOLICY_DIRS += \
+    device/samsung/i9100g/sepolicy
 
-BOARD_SEPOLICY_UNION := \
+BOARD_SEPOLICY_UNION += \
     file_contexts \
-    file.te \
-    device.te \
-    domain.te \
-    pvrsrvinit.te \
-    rild.te \
-    wpa_supplicant.te
+    mediaserver.te \
+    system_app.te \
+    system_server.te \
+    tvout_service.te
 
 # Recovery
 TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
