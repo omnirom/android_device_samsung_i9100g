NamelessRom/android_device_samsung_i9300

audio: forward call mute to RIL client interface
Change-Id: Iccb763b48a687b88692c84976a7cddf72031b5be

audio/ril_interface.c
@@ -38,6 +38,7 @@ int (*_ril_set_call_volume)(void *, enum ril_sound_type, int);
 int (*_ril_set_call_audio_path)(void *, enum ril_audio_path);
 int (*_ril_set_call_clock_sync)(void *, enum ril_clock_state);
 int (*_ril_set_two_mic_control)(void *, enum ril_two_mic_device, enum ril_two_mic_state);
+int (*_ril_set_mic_mute)(void *, enum ril_mic_mute);
 int (*_ril_register_unsolicited_handler)(void *, int, void *);
 int (*_ril_get_wb_amr)(void *, void *);
 
@@ -108,6 +109,7 @@ int ril_open(struct ril_handle *ril)
     _ril_set_call_audio_path = dlsym(ril->handle, "SetCallAudioPath");
     _ril_set_call_clock_sync = dlsym(ril->handle, "SetCallClockSync");
     _ril_set_two_mic_control = dlsym(ril->handle, "SetTwoMicControl");
+    _ril_set_mic_mute = dlsym(ril->handle, "SetMute");
     _ril_register_unsolicited_handler = dlsym(ril->handle,
                                               "RegisterUnsolicitedHandler");
     /* since this function is not supported in all RILs, don't require it */
@@ -115,8 +117,8 @@ int ril_open(struct ril_handle *ril)
 
     if (!_ril_open_client || !_ril_close_client || !_ril_connect ||
         !_ril_is_connected || !_ril_disconnect || !_ril_set_call_volume ||
-        !_ril_set_call_audio_path || !_ril_set_two_mic_control || !_ril_set_call_clock_sync ||
-        !_ril_register_unsolicited_handler) {
+        !_ril_set_call_audio_path || !_ril_set_two_mic_control || !_ril_set_mic_mute ||
+        !_ril_set_call_clock_sync || !_ril_register_unsolicited_handler) {
         ALOGE("Cannot get symbols from '%s'", RIL_CLIENT_LIBPATH);
         dlclose(ril->handle);
         return -1;
@@ -190,4 +192,12 @@ int ril_set_two_mic_control(struct ril_handle *ril, enum ril_two_mic_device devi
         return 0;
 
     return _ril_set_two_mic_control(ril->client, device, state);
+}
+
+int ril_set_mic_mute(struct ril_handle *ril, enum ril_mic_mute state)
+{
+    if (ril_connect_if_required(ril))
+        return 0;
+
+    return _ril_set_mic_mute(ril->client, state);
 }

omnirom/android_device_samsung_i9100g

audio/ril_interface.c
@@ -37,6 +37,7 @@ int (*_ril_disconnect)(void *);
 int (*_ril_set_call_volume)(void *, enum ril_sound_type, int);
 int (*_ril_set_call_audio_path)(void *, enum ril_audio_path);
 int (*_ril_set_two_mic_control)(void *, enum ril_two_mic_device, enum ril_two_mic_state);
+int (*_ril_set_mic_mute)(void *, enum ril_mic_mute);
 int (*_ril_register_unsolicited_handler)(void *, int, void *);
 int (*_ril_get_wb_amr)(void *, void *);
 
@@ -106,6 +107,7 @@ int ril_open(struct ril_handle *ril)
     _ril_set_call_volume = dlsym(ril->handle, "SetCallVolume");
     _ril_set_call_audio_path = dlsym(ril->handle, "SetCallAudioPath");
     _ril_set_two_mic_control = dlsym(ril->handle, "SetTwoMicControl");
+    _ril_set_mic_mute = dlsym(ril->handle, "SetMute");
     _ril_register_unsolicited_handler = dlsym(ril->handle,
                                               "RegisterUnsolicitedHandler");
     /* since this function is not supported in all RILs, don't require it */
@@ -113,7 +115,8 @@ int ril_open(struct ril_handle *ril)
 
     if (!_ril_open_client || !_ril_close_client || !_ril_connect ||
         !_ril_is_connected || !_ril_disconnect || !_ril_set_call_volume ||
-        !_ril_set_call_audio_path || !_ril_set_two_mic_control || !_ril_register_unsolicited_handler) {
+        !_ril_set_call_audio_path || !_ril_set_two_mic_control || !_ril_register_unsolicited_handler ||
+        !_ril_set_mic_mute) {
         ALOGE("Cannot get symbols from '%s'", RIL_CLIENT_LIBPATH);
         dlclose(ril->handle);
         return -1;
@@ -179,4 +182,12 @@ int ril_set_two_mic_control(struct ril_handle *ril, enum ril_two_mic_device devi
         return 0;
 
     return _ril_set_two_mic_control(ril->client, device, state);
+}
+
+int ril_set_mic_mute(struct ril_handle *ril, enum ril_mic_mute state)
+{
+    if (ril_connect_if_required(ril))
+        return 0;
+
+    return _ril_set_mic_mute(ril->client, state);
 }